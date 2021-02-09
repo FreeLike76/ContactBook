74eac6d9dfb548d5c64a7e68a6f48a946cac8b81
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2021-02-09 12:31:51 +0200

    Added Contact class, created an ArrayList of Objects, and 2 functions to operate with list of contacts.

---
+++ src/com/company/Contact.java
@@ -0,0 +1,91 @@
+package com.company;
+
+public class Contact {
+    private String lastName;
+    private String firstName;
+    private String patronymic;
+    private String addressName;
+    private int addressNumber;
+    private String phoneNumber;
+    private String info;
+
+    Contact(String lastName, String firstName, String patronymic,
+            String addressName, int addressNumber, String phoneNumber, String info) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.patronymic = patronymic;
+        this.addressName = addressName;
+        this.addressNumber = addressNumber;
+        this.phoneNumber = phoneNumber;
+        this.info = info;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getAddressName() {
+        return addressName;
+    }
+
+    public void setAddressName(String addressName) {
+        this.addressName = addressName;
+    }
+
+    public int getAddressNumber() {
+        return addressNumber;
+    }
+
+    public void setAddressNumber(int addressNumber) {
+        this.addressNumber = addressNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", addressName='" + addressName + '\'' +
+                ", addressNumber=" + addressNumber +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", info='" + info + '\'' +
+                '}';
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,56 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Contact> ContactBook = new ArrayList<>(Arrays.asList(
+                new Contact("Geleshko", "Dmytro", "Romanovych",
+                        "Kiev, Vyborzka", 3, "+380612357608", "Author"),
+                new Contact("Mamontov", "Vadym", "Sergiyovych",
+                        "Rivne, Lypneva", 12, "+380671712309", "Student"),
+                new Contact("Melnyk", "Vasyl", "Stepanovych",
+                        "Rivne, Naberegna", 2, "", "Friend"),
+                new Contact("Migirov", "Ivan", "Dmytrovych",
+                        "Lviv, Pivnichna", 8, "+380681757608", "Friend"),
+                new Contact("Samoliuk", "Michael", "Romanovych",
+                        "Rivne, Central", 41, "+380671757606", "Teacher"),
+                new Contact("Melnyk", "Stepan", "Vadymovych",
+                        "Kiev, Orlova", 2, "+380444457605", "Student"),
+                new Contact("Zlaman", "Maxim", "Ivanovych",
+                        "Odesa, Zahidna", 5, "+380612227603", "Student"),
+                new Contact("Kaida", "Ann", "Stepanovych",
+                        "Kiev, Vyborzka", 3, "", "Doctor"),
+                new Contact("Gulchak", "Sophia", "Romanovych",
+                        "Rivne, Central", 8, "+380671733308", "Student"),
+                new Contact("Orlovsky", "Roman", "Ivanovych",
+                        "Kiev, Malunova", 44, "+380688997609", "Doctor")));
+
+        System.out.println("Contacts by char:");
+        System.out.println(getContactByChar(ContactBook, 'D'));
+        System.out.println(getContactByChar(ContactBook, 'R'));
+        System.out.println("Contacts with phones:");
+        System.out.println(getContactsWithPhones(ContactBook));
+    }
+
+    static ArrayList<Contact> getContactByChar(ArrayList<Contact> ContactBook, char ch) {
+        ArrayList<Contact> result = new ArrayList<>();
+        for (Contact con : ContactBook) {
+            if(con.getFirstName().charAt(0)==ch) {
+                result.add(con);
+            }
+        }
+        return result;
+    }
+    static ArrayList<Contact> getContactsWithPhones(ArrayList<Contact> ContactBook) {
+        ArrayList<Contact> result = new ArrayList<>();
+        for (Contact con : ContactBook) {
+            if(!(con.getPhoneNumber().isEmpty() || con.getPhoneNumber().isBlank())) {
+                result.add(con);
+            }
+        }
+        return result;
     }
 }
